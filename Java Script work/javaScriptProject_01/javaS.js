d155e0ad3620b9a797a5174e9cb95aaeb981cf4a
Md. Imrul Kayes <md.tushar1804@gmail.com>
2020-11-13 23:56:51 +0600

    commit-2 13/11/2020

---
+++ Java Script work/javaScriptProject_01/javaS.js
@@ -0,0 +1,14 @@
+var Fname = prompt("Frist Name");
+//console.log(Fname);
+var Lname = prompt("Last Name");
+// console.log(Lname);
+var ang = prompt("your age");
+// console.log(ang);
+var height = prompt("how Tall? ");
+// console.log(height);
+var Nname = prompt("What is your Pet Name? ");
+// console.log(Nname);
+
+if(Fname[0]==Lname[0] && ang>=20 && ang<=30 && height>170 && 'y'==Nname[Nname.length-1]){
+    console.log("Syp");
+}
---
+++ Java Script work/task1/javascript1.js
@@ -0,0 +1,3 @@
+var  value = prompt("what is the weight in pounds (lls)");
+alert("That is "+value*0.454+" kilograms");
+console.log("successfully");
